0cd1b1677dd4a1cb53e321b5f507d68d8fbb40c5
parisi18 <parisi18@unifesp.br>
2025-01-11 02:47:46 -0300

    feat: banner component

---
+++ organo/src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/images/banner.png" alt="Main Banner" />
+        </header>
+    )
+}
+
+export default Banner
